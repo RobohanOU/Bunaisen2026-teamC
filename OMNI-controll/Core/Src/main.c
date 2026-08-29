ec7088878fddb6c76702d87adaf3407afb4950ea
Standard8989 <newstandard.first@gmail.com>
2026-08-29 23:45:23 +0900

    フォーマット

--- OMNI-controll/Core/Src/main.c
+++ OMNI-controll/Core/Src/main.c
@@ -190,57 +190,49 @@ int main(void) {
 	/* USER CODE BEGIN WHILE */
 	while (1) {
 		// のこのこ
-		float r=1.0; //半径が分かり次第いれます
-		float L=1.0; //機体の中心からホイールまでの距離が分かりしだいいれます
-		float v_x=DS4.r_stick_x / 128.0;//全部-1から1に圧縮してます
-		float v_y=DS4.r_stick_y / 128.0;
-		float omega=DS4.l_stick_x /128.0;
-		float omega_1=(-0.707*v_x+0.707*v_y+L*omega)/(1.414+L);
-		float omega_2=(0.707*v_x+0.707*v_y+L*omega)/(1.414+L);
-		float omega_3=(0.707*v_x-0.707*v_y+L*omega)/(1.414+L);
-		float omega_4=(-0.707*v_x-0.707*v_y+L*omega)/(1.414+L);
+		float r = 1.0; //半径が分かり次第いれます
+		float L = 1.0; //機体の中心からホイールまでの距離が分かりしだいいれます
+		float v_x = DS4.r_stick_x / 128.0; //全部-1から1に圧縮してます
+		float v_y = DS4.r_stick_y / 128.0;
+		float omega = DS4.l_stick_x / 128.0;
+		float omega_1 = (-0.707 * v_x + 0.707 * v_y + L * omega) / (1.414 + L);
+		float omega_2 = (0.707 * v_x + 0.707 * v_y + L * omega) / (1.414 + L);
+		float omega_3 = (0.707 * v_x - 0.707 * v_y + L * omega) / (1.414 + L);
+		float omega_4 = (-0.707 * v_x - 0.707 * v_y + L * omega) / (1.414 + L);
 		if (omega_1 >= 0 & omega_1 <= 1) {
 			__HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_1, 500 * omega_1);
 			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
-		}
-		else if (omega_1 < 0 & omega_1 >=-1){
+		} else if (omega_1 < 0 & omega_1 >= -1) {
 			__HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_1, 500 * -omega_1);
 			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
-		}
-		else{
+		} else {
 
 		}
 		if (omega_2 >= 0 & omega_2 <= 1) {
 			__HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_3, 500 * omega_2);
 			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
-		}
-		else if (omega_2 < 0 & omega_2 >=-1){
+		} else if (omega_2 < 0 & omega_2 >= -1) {
 			__HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_3, 500 * -omega_2);
 			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
-		}
-		else{
+		} else {
 
 		}
 		if (omega_3 >= 0 & omega_3 <= 1) {
 			__HAL_TIM_SET_COMPARE(&htim12, TIM_CHANNEL_1, 500 * omega_3);
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_SET);
-		}
-		else if (omega_3 < 0 & omega_3 >=-1){
+		} else if (omega_3 < 0 & omega_3 >= -1) {
 			__HAL_TIM_SET_COMPARE(&htim12, TIM_CHANNEL_1, 500 * -omega_3);
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_RESET);
-		}
-		else{
+		} else {
 
 		}
 		if (omega_4 >= 0 & omega_4 <= 1) {
 			__HAL_TIM_SET_COMPARE(&htim15, TIM_CHANNEL_1, 500 * omega_4);
 			HAL_GPIO_WritePin(GPIOE, GPIO_PIN_6, GPIO_PIN_SET);
-		}
-		else if (omega_4 < 0 & omega_4 >=-1){
+		} else if (omega_4 < 0 & omega_4 >= -1) {
 			__HAL_TIM_SET_COMPARE(&htim15, TIM_CHANNEL_1, 500 * -omega_4);
 			HAL_GPIO_WritePin(GPIOE, GPIO_PIN_6, GPIO_PIN_RESET);
-		}
-		else{
+		} else {
 
 		}
 		/* USER CODE END WHILE */
